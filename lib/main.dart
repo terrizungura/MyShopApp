19e12b29397f608119fdd36fee856d1d7bc5d1ef
Tererai Zungura <tererai@cryptcode.info>
2020-06-11 11:35:44 +0200

    Implemented the filtering for favorite items. Used Stateful widget of overview

--- lib/main.dart
+++ lib/main.dart
@@ -12,14 +12,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (ctx) => Products(),
-          child: MaterialApp(
+    return ChangeNotifierProvider.value(
+      value: Products(),
+      child: MaterialApp(
         title: 'MyShop',
-        theme: ThemeData( primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrange,
-        fontFamily: 'Lato'
-           ),
+        theme: ThemeData(
+            primarySwatch: Colors.purple,
+            accentColor: Colors.deepOrange,
+            fontFamily: 'Lato'),
         home: ProductsOverviewScreen(),
         routes: {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
@@ -28,4 +28,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -12,8 +12,7 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context, listen: true);
-    print("rtgfs");
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
         borderRadius: BorderRadius.circular(10),
         child: GridTile(        
@@ -25,7 +24,7 @@ class ProductItem extends StatelessWidget {
           footer: GridTileBar(
             backgroundColor: Colors.black87,
             leading: Consumer<Product>(
-      builder: (ctx, product, child) =>IconButton(
+      builder: (ctx, product, _) =>IconButton(
               icon: Icon(product.isFavorite ?  Icons.favorite : Icons.favorite_border),
               onPressed: () {
                 product.toggleFavoriteStatus();
