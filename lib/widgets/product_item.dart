012fda05931428854e5ffe241f5b68a5f3c9d119
Tererai Zungura <tererai@cryptcode.info>
2020-06-11 10:16:19 +0200

    Using the Consumer widget in place of the Provider. Consumer has the child argument that allows you to exclude the widgets that you dont want to rebuild

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -12,32 +12,37 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: true);
+    print("rtgfs");
     return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(        
-        child: GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: product.id);
-            },
-            child: Image.network(product.imageUrl, fit: BoxFit.cover)),
-        footer: GridTileBar(
-          backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(product.isFavorite ?  Icons.favorite : Icons.favorite_border),
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
-            color: Theme.of(context).accentColor,
-          ),
-          title: Text(product.title, textAlign: TextAlign.center),
-          trailing: IconButton(
-            icon: Icon(Icons.shopping_cart),
-            onPressed: () {},
-            color: Theme.of(context).accentColor,
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(        
+          child: GestureDetector(
+              onTap: () {
+                Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: product.id);
+              },
+              child: Image.network(product.imageUrl, fit: BoxFit.cover)),
+          footer: GridTileBar(
+            backgroundColor: Colors.black87,
+            leading: Consumer<Product>(
+      builder: (ctx, product, child) =>IconButton(
+              icon: Icon(product.isFavorite ?  Icons.favorite : Icons.favorite_border),
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+              color: Theme.of(context).accentColor,
+            ),
+            ),
+            title: Text(product.title, textAlign: TextAlign.center),
+            trailing: IconButton(
+              icon: Icon(Icons.shopping_cart),
+              onPressed: () {},
+              color: Theme.of(context).accentColor,
+            ),
           ),
         ),
-      ),
+      
+          
     );
   }
 }
