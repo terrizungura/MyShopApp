9672c2bf655c45e4f2f0fd846f76169da4e03ea2
Tererai Zungura <tererai@cryptcode.info>
2020-06-12 10:27:12 +0200

    Display items in the cart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../providers/cart.dart';
+import '../providers/cart.dart' show Cart;
+import '../widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -35,10 +36,25 @@ class CartScreen extends StatelessWidget {
                     ),
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
-                  FlatButton(onPressed: (){}, child: Text('ORDER NOW'), textColor: Theme.of(context).primaryColor,),
+                  FlatButton(
+                    onPressed: () {},
+                    child: Text('ORDER NOW'),
+                    textColor: Theme.of(context).primaryColor,
+                  ),
                 ]),
           ),
-        )
+        ),
+        SizedBox(height: 10),
+        Expanded(
+          child: ListView.builder(
+            itemCount: cart.itemCount,
+            itemBuilder: (ctx, i) => CartItem(
+                cart.items.values.toList()[i].id,
+                cart.items.values.toList()[i].price,
+                cart.items.values.toList()[i].quantity,
+                cart.items.values.toList()[i].title),
+          ),
+        ),
       ]),
     );
   }
