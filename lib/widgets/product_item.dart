87cc06d59c6fdba018ed4b0d4ec5bb3c866bd220
Tererai Zungura <terrizun@gmail.com>
2020-06-18 15:25:02 +0200

    Get request implementation

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -22,7 +22,7 @@ class ProductItem extends StatelessWidget {
               onTap: () {
                 Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: product.id);
               },
-              child: Image.network(product.imageUrl, fit: BoxFit.cover)),
+              child: Image.network(product.imageUrl, fit: BoxFit.cover),),
           footer: GridTileBar(
             backgroundColor: Colors.black87,
             leading: Consumer<Product>(
