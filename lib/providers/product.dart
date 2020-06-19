bd924be4fec6f975a58ea5fe5ba5a6d391a96eac
Tererai Zungura <terrizun@gmail.com>
2020-06-19 09:05:50 +0200

    Updating 'favorite' status optimistically

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,25 +1,46 @@
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Product with ChangeNotifier{
+class Product with ChangeNotifier {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
 
-final String id;
-final String title;
-final String description;
-final double price;
-final String imageUrl;
-bool isFavorite;
+  Product({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.price,
+    @required this.imageUrl,
+    this.isFavorite = false,
+  });
 
-Product({
-@required this.id,
-@required this.title,
-@required this.description,
-@required this.price,
-@required this.imageUrl,
-this.isFavorite=false,
-});
+  void _setFavValue(bool newValue){
+    isFavorite = newValue;
+      notifyListeners();
+  }
 
-void toggleFavoriteStatus(){
-  isFavorite = !isFavorite;
-  notifyListeners();
-}
+  void toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
+    isFavorite = !isFavorite;
+    notifyListeners();
+    final url = 'https://flutterproj-847d6.firebaseio.com/products/$id.json';
+    try {
+      final response =await http.patch(
+        url,
+        body: json.encode({
+          'isfavorite': isFavorite,
+        }),
+      );
+      if(response.statusCode >= 400){
+      _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
+  }
 }
