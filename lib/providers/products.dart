988323a232a69243b56809357ed46b0c72c61ac2
Tererai Zungura <terrizun@gmail.com>
2020-06-23 09:18:42 +0200

    Attaching products to users

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -73,8 +73,9 @@ class Products with ChangeNotifier {
   //   notifyListeners();
   // }
 
-  Future<void> fetchAndSetProducts() async {
-    var url = 'https://flutterproj-847d6.firebaseio.com/products.json?auth=$authToken';
+  Future<void> fetchAndSetProducts([bool filterByUser = false]) async {
+    final filterString = filterByUser? 'orderBy="creatorId"&equalTo="$userId"' : '';
+    var url = 'https://flutterproj-847d6.firebaseio.com/products.json?auth=$authToken&filterString';
     try {
       final response = await http.get(url);
       final extractedData = json.decode(response.body) as Map<String, dynamic>;
@@ -112,6 +113,7 @@ class Products with ChangeNotifier {
           'description': product.description,
           'imageUrl': product.imageUrl,
           'price': product.price,
+          'creatorId': userId,
         }),
       );
       final newProduct = Product(
